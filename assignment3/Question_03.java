da5572567f3514768d6a7f7daaf24bd4941d8fc1
saksham0612 <sakshamkatyal10@gmail.com>
2024-11-12 00:40:46 +0530

    assignment3

---
+++ assignment3/Question_03.java
@@ -0,0 +1,23 @@
+package com.sunbeam.assignment3;
+
+public class Question_03 {
+	
+public static void main(String[] args) {
+		
+		LinkedList l1 = new LinkedList();
+		
+		l1.addFirst(40);
+		l1.addFirst(30);
+		l1.addFirst(20);
+		l1.addFirst(10);
+		l1.addLast(50);
+		l1.addLast(60);
+		l1.deleteFirst();
+		l1.deleteLast();
+		l1.addMid(100, 1);
+		l1.deleteMid(3);
+		l1.display();
+
+	}
+
+}
